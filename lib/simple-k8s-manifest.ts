5f67a7ec01f2effe4302c4a00a33adb0eedca0da
jj <jj@ondemandenv.dev>
2025-02-18 11:31:07 -0500

    more k8s manifest illustrates Application-Centric Infrastructure

--- lib/simple-k8s-manifest.ts
+++ lib/simple-k8s-manifest.ts
@@ -1,18 +1,43 @@
-import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import {Stack, StackProps} from 'aws-cdk-lib';
+import {Construct} from 'constructs';
 import {Cluster} from "aws-cdk-lib/aws-eks";
 import {StringParameter} from "aws-cdk-lib/aws-ssm";
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'; // Import DynamoDB module
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as kplus from 'cdk8s-plus-31';
+import * as cdk8s from 'cdk8s';
 
 
-
+/**
+ * this stack illustrates Application-Centric Infrastructure:
+ *     resources k8s deployment inside EKs cluster
+ *     resources the dynamodb table outside
+ * are logically same application,
+ * and dependencies are maintained by Cloudformation
+ *
+ *
+ * Note:
+ * 1) this stack is not fully working yet as AWS CDK's EKS and cdk8s support is still under development ...
+ *      a) VPC, SG, IAM all have to match exactly to make Cluster.fromClusterAttributes work, and have to be in same account/region.
+ *      b) all dynamic values like imageAndVersion are not supported, because the
+ *         https://github.com/aws/aws-cdk/tree/main/packages/%40aws-cdk/custom-resource-handlers/lib/aws-eks/kubectl-handler
+ *         is implemented completely wrong.
+ *
+ * 2) ondemandenv's implementation support cross accounts k8s manifest deployment with dynamic values:
+ * EKsManifest https://github.com/ondemandenv/odmd-contracts-base/blob/main/lib/model/odmd-eks-manifest.ts
+ *
+ */
 export class SimpleK8sManifestStack extends Stack {
     constructor(scope: Construct, id: string, props: StackProps & { cluster: Cluster }) {
         super(scope, id, props);
 
-        const cluster = props.cluster;
+        // const cluster = props.cluster;// this is going to deploy manifest to the cluster's stack!
+        const cluster = Cluster.fromClusterAttributes(this, 'my_cluster', {
+            clusterName: props.cluster.clusterName,
+            kubectlRoleArn: props.cluster.kubectlRole?.roleArn,
+            vpc: props.cluster.vpc,
+            // probably need more to make this work
+        })
 
-        // Define Kubernetes Manifest
         const appNamespace = cluster.addManifest('app-namespace', {
             apiVersion: 'v1',
             kind: 'Namespace',
@@ -21,53 +46,16 @@ export class SimpleK8sManifestStack extends Stack {
             },
         });
 
-        // Load  image version from Parameter Store
         const imageAndVersion = StringParameter.valueForStringParameter(this, '/my-app/nginx-image-and-version');
 
         // Define DynamoDB Table
         const dynamoTable = new dynamodb.Table(this, 'MyAppTable', {
-            partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+            partitionKey: {name: 'id', type: dynamodb.AttributeType.STRING},
         });
 
-
-        const appDeployment = cluster.addManifest('app-deployment', {
-            apiVersion: 'apps/v1',
-            kind: 'Deployment',
-            metadata: {
-                namespace: 'my-app-namespace',
-                name: 'my-app-deployment',
-            },
-            spec: {
-                replicas: 3,
-                selector: {
-                    matchLabels: {
-                        app: 'my-app',
-                    },
-                },
-                template: {
-                    metadata: {
-                        labels: {
-                            app: 'my-app',
-                        },
-                    },
-                    spec: {
-                        containers: [
-                            {
-                                name: 'my-app-container',
-                                image: `${imageAndVersion}`,
-                                ports: [{ containerPort: 80 }],
-                                env: [
-                                    {
-                                        name: 'DYNAMODB_TABLE_NAME',
-                                        value: dynamoTable.tableName
-                                    },
-                                ],
-                            },
-                        ],
-                    },
-                },
-            },
-        });
+        const appDeployment = cluster.addManifest('app-deployment',
+            this.createManifest(imageAndVersion, dynamoTable.tableName)
+        );
 
         const appService = cluster.addManifest('app-service', {
             apiVersion: 'v1',
@@ -80,7 +68,7 @@ export class SimpleK8sManifestStack extends Stack {
                 selector: {
                     app: 'my-app',
                 },
-                ports: [{ port: 80, targetPort: 80 }],
+                ports: [{port: 80, targetPort: 80}],
                 type: 'LoadBalancer',
             },
         });
@@ -89,4 +77,25 @@ export class SimpleK8sManifestStack extends Stack {
         appDeployment.node.addDependency(appNamespace);
         appService.node.addDependency(appDeployment);
     }
+
+
+    //https://cdk8s.io/docs/latest/plus/cdk8s-plus-31/deployment/
+    private createManifest(imageAndVersion: string, tableName: string) {
+        const app = new cdk8s.App()
+        const chart = new cdk8s.Chart(app, 'MyAppTable')
+
+        new kplus.Deployment(chart, 'MyAppTable', {
+            containers: [
+                {
+                    name: 'my-app-container',
+                    image: `${imageAndVersion}`,
+                    ports: [{number: 80}],
+                    envVariables: {
+                        DYNAMODB_TABLE_NAME: {value: tableName},
+                    }
+                },]
+        })
+        return chart.toJson()
+    }
+
 }
